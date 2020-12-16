b08711751fd727278a4b6ee2c154327470a42866
Hanna Murai <hannamurai@Hannas-MacBook-Pro.local>
2020-12-16 10:03:23 -0600

    aged-order

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const Shopify = require("shopify-api-node");
 let date = Date.now()
 
-const shopify1 = new Shopify({
-    shopName: "wigscom",
-   
-  autoLimit: true,
-  bucketSize: { calls: 5, interval: 1000, bucketSize: 35 },
-});
+const express = require("express");
+const http = require("https");
+const app = express();
+
+const port = 3001;
+
+
 
 async function getData (store, id, days) {
     let shopify = {}
 if(store === 'wigscom') {
     shopify = shopify1
 }
+if(store === 'wigs-hairpieces') {
+    shopify = shopify2
+}
+
 let order = await shopify.order.get(id)
     const oneDay = 24 * 60 * 60 * 1000; 
     const date2 = new Date(order.created_at);
@@ -23,7 +28,17 @@ let order = await shopify.order.get(id)
 
    
 }
-Promise.all( [getData('wigscom', 2644120305775, 10)]).then(res => {
-console.log({result: res[0]})
-})
 
+
+app.get("/site=:site/id=:id/age=:age", ( req, res, next) => {
+ 
+    Promise.all( [getData(req.params.site, req.params.id, req.params.age)]).then(result => {
+        console.log({result: result[0]})
+        res.send({result: result[0]});
+        }).catch(next)
+    
+  })
+  
+  app.listen(port, () =>
+    console.log(`Hello world app listening on port ${port}!`)
+  );
